5a3b4376fbf3c69edee20b41b63509c01098dc7a
deepaknairrpf <cse15b016@iiitk.ac.in>
2016-05-23 12:10:35 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,18 @@
+
+public class sum {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number1 = 4;
+	 float number2 = 5.6f;
+	   int sum;
+	   sum = (int)(number1 + number2);
+	   System.out.println(sum);
+	 
+	}
+
+}
